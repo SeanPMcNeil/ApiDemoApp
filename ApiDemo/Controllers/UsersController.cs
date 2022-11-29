8f6f05cc5e7f19f414d70bffc082320f93937912
Sean McNeil <smcneil136@gmail.com>
2022-11-29 11:51:20 -0500

    C# Web API Demo

---
+++ ApiDemo/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController (ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/values
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
---
+++ ApiDemo/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ApiDemo.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+    }
+}
+
